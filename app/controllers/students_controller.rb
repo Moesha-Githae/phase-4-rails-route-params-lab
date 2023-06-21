e6d88fa54d1b9d84d9cd33fded0ea6fdc14fbb92
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-06-21 14:41:28 +0300

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
+
   def index
-    students = Student.all
+
+
+    students = if params[:name]
+                  Student.find_by(first_name: params[:name].downcase.titleize) || Student.find_by(last_name: params[:name].downcase.titleize)
+              else
+                Student.all
+              end
     render json: students
+    # byebug
   end
+
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 
 end
